89ab47f12baf1fc0d94e6c2eeb43f8d26422f885
Lena K <e.kuletskaia@gmail.com>
2024-03-27 16:03:22 -0400

    Update of hw2_testcase1

--- hw2_testcase1.py
+++ hw2_testcase1.py
@@ -14,10 +14,11 @@ driver.get('https://www.target.com/')
 
 # steps
 driver.find_element(By.XPATH, '//span[text()="Sign in"]').click()
-driver.find_element(By.XPATH, '//span[@class="styles__ListItemText-sc-diphzn-1 jaMNVl" and text()="Sign in"]').click()
+driver.find_element(By.XPATH, '//a[@data-test="accountNav-signIn"]').click()
 
 # verification
 actual_text = driver.find_element(By.XPATH, '//span[text()="Sign into your Target account"]').text
+driver.find_element(By.ID, "login")
 #driver.find_element(By.ID, "login")
 assert "Sign into your Target account" in actual_text, f"Error! -Sign into your Target account- not in {actual_text}"
 print("Test case passed.")
